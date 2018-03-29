23a73d2f5579b96e1699d76ad5c54d5e724eaaa1
hendySS <hendysantoso911@gmail.com>
2018-03-29 10:46:06 +0700

    stage4

--- controller/accountController.js
+++ controller/accountController.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose'),
 User = mongoose.model('users');
 
+var users = new User({
+    _id:new mongoose.Types.ObjectId(),
+    nama:"user1",
+  }); 
+  
+users.save(function(err,projects){
+    console.log('ok');
+});
+
 exports.todo1 = function(req, res){
     User.find({}, function(err, penduduk) {
         if (err){
