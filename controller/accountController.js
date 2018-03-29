29aaa1a2270af45f6a5f859837dbf4b9326bdb08
hendySS <hendysantoso911@gmail.com>
2018-03-29 10:43:57 +0700

    stage3

--- controller/accountController.js
+++ controller/accountController.js
@@ -1,2 +1,12 @@
 var mongoose = require('mongoose'),
 User = mongoose.model('users');
+
+exports.todo1 = function(req, res){
+    User.find({}, function(err, penduduk) {
+        if (err){
+            res.send(err);
+        }
+
+        res.json(penduduk);
+    });
+}
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,3 +1,7 @@
 module.exports = function(app){
 
+    var todoList = require("../controller/accountController");
+
+    app.route('/hello')
+        .get(todoList.todo1);
 }
