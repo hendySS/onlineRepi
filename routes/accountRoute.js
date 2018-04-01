283faa8ffd1f8de43ac7baa57b7da3a898377919
hendySS <hendysantoso911@gmail.com>
2018-04-01 17:32:03 +0700

    revisi3

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,7 +1,10 @@
 module.exports = function(app){
 
     var todoList = require("../controller/accountController");
-    
+
     app.route('/')
+        .get(todoList.homePage);
+
+    app.route('/hello')
         .get(todoList.todo1);
 }
